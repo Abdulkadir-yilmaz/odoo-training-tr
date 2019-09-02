5b6135b82a68adf5c597e6b2716e77dd1e6c113a
Abdulkadir-yilmaz <abdlkdrylmz@gmail.com>
2019-09-02 16:54:23 +0300

    odoo-training certification module

---
+++ certification/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Certification',
+    'summary': "Defines certification for different purposes.",
+    'author': "Abdulkadir-yilmaz",
+    'website': "https://github.com/abdulkadir-yilmaz",
+    'category': 'Certification Management',
+    'version': '12.0.1.0.0',
+    'License': 'AGPL-3',
+    'depends': ['base'],
+    'data': ['security/ir.model.access.csv',
+            'views/certification_view.xml',
+            'views/res_partner_view.xml',
+            'views/standard_view.xml'
+             ],
+    'development_satus' : 'Beta',
+    'maintainers' : ['abdulkadir-yilmaz'],
+}
